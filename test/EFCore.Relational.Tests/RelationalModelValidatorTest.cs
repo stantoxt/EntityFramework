8a433e4a14ba1090ff5c713bbe12ce6b06669baf
Smit Patel <smitpatel@users.noreply.github.com>
2017-06-12 17:21:27 -0700

    Remove unnecessary using statements

--- test/EFCore.Relational.Tests/RelationalModelValidatorTest.cs
+++ test/EFCore.Relational.Tests/RelationalModelValidatorTest.cs
@@ -4,7 +4,6 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Internal;
